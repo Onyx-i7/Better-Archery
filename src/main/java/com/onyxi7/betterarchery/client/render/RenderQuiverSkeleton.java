d45d3aa481e28735633eab5e18ab5501128ef41d
Onyx_i7 <Crackdroid69@gmail.com>
2026-07-01 01:06:38 -0400

    Refactor RenderQuiverSkeleton to use AbstractSkeleton

--- src/main/java/com/onyxi7/betterarchery/client/render/RenderQuiverSkeleton.java
+++ src/main/java/com/onyxi7/betterarchery/client/render/RenderQuiverSkeleton.java
@@ -3,7 +3,7 @@ package com.onyxi7.betterarchery.client.render;
 import com.onyxi7.betterarchery.entities.EntityQuiverSkeleton;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderSkeleton;
-import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.monster.AbstractSkeleton;
 import net.minecraft.util.ResourceLocation;
 
 public class RenderQuiverSkeleton extends RenderSkeleton {
@@ -15,8 +15,7 @@ public class RenderQuiverSkeleton extends RenderSkeleton {
         this.addLayer(new LayerQuiver(this));
     }
     
-    @Override
-    protected ResourceLocation getEntityTexture(EntitySkeleton entity) {
+    protected ResourceLocation getEntityTexture(AbstractSkeleton entity) {
         return TEXTURE;
     }
 }
