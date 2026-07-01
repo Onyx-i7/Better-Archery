75d9a7e396e1276ab14cc4b4f38049b79cdd5df9
Onyx_i7 <Crackdroid69@gmail.com>
2026-06-30 22:15:44 -0400

    Refactor player layer registration in ClientProxy

--- src/main/java/com/onyxi7/betterarchery/proxy/ClientProxy.java
+++ src/main/java/com/onyxi7/betterarchery/proxy/ClientProxy.java
@@ -3,11 +3,7 @@ package com.onyxi7.betterarchery.proxy;
 import com.onyxi7.betterarchery.client.render.LayerQuiver;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.entity.RenderLivingBase;
-import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -28,13 +24,8 @@ public class ClientProxy extends CommonProxy {
     }
     
     private void registerPlayerLayers() {
-        RenderManager renderManager = Minecraft.getMinecraft().getRenderManager();
-        
-        for (Render<? extends EntityPlayer> render : renderManager.getEntityRenderMap().values()) {
-            if (render instanceof RenderPlayer) {
-                RenderPlayer renderPlayer = (RenderPlayer) render;
-                renderPlayer.addLayer(new LayerQuiver(renderPlayer));
-            }
+        for (RenderPlayer renderPlayer : Minecraft.getMinecraft().getRenderManager().getSkinMap().values()) {
+            renderPlayer.addLayer(new LayerQuiver(renderPlayer));
         }
     }
 }
