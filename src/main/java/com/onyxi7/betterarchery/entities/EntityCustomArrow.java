0376d94c7bc1be3e11cf354b206928175a9952ba
Onyx_i7 <Crackdroid69@gmail.com>
2026-07-02 20:32:07 -0400

    Add bowStack field and NBT serialization for arrows
    Added bowStack field to store the bow that shot the arrow. Implemented NBT serialization for bowStack.

--- src/main/java/com/onyxi7/betterarchery/entities/EntityCustomArrow.java
+++ src/main/java/com/onyxi7/betterarchery/entities/EntityCustomArrow.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
@@ -20,6 +21,9 @@ public abstract class EntityCustomArrow extends EntityArrow {
     
     protected int homingTicks = 0;
     protected static final int MAX_HOMING_TICKS = 40; // 2 seconds
+    
+    // Store the bow that shot this arrow
+    protected ItemStack bowStack = ItemStack.EMPTY;
     
     public EntityCustomArrow(World worldIn) {
         super(worldIn);
@@ -33,26 +37,22 @@ public abstract class EntityCustomArrow extends EntityArrow {
         super(worldIn, x, y, z);
     }
     
+    // Set the bow that shot this arrow
+    public void setBowStack(ItemStack bow) {
+        this.bowStack = bow.copy();
+    }
+    
     @Override
     public void onUpdate() {
         super.onUpdate();
         
         // Apply homing if enchantment is present
-        if (!this.world.isRemote && this.shootingEntity instanceof EntityLivingBase) {
-            EntityLivingBase shooter = (EntityLivingBase) this.shootingEntity;
-            ItemStack bowStack = shooter.getHeldItemMainhand();
+        if (!this.world.isRemote && !bowStack.isEmpty()) {
+            int homingLevel = EnchantmentHelper.getEnchantmentLevel(EnchantmentInit.HOMING, bowStack);
             
-            if (bowStack.isEmpty() || !(bowStack.getItem() instanceof ItemBow)) {
-                bowStack = shooter.getHeldItemOffhand();
-            }
-            
-            if (!bowStack.isEmpty()) {
-                int homingLevel = EnchantmentHelper.getEnchantmentLevel(EnchantmentInit.HOMING, bowStack);
-                
-                if (homingLevel > 0 && homingTicks < MAX_HOMING_TICKS) {
-                    applyHoming(homingLevel);
-                    homingTicks++;
-                }
+            if (homingLevel > 0 && homingTicks < MAX_HOMING_TICKS) {
+                applyHoming(homingLevel);
+                homingTicks++;
             }
         }
     }
@@ -74,18 +74,15 @@ public abstract class EntityCustomArrow extends EntityArrow {
         double minDistance = Double.MAX_VALUE;
         
         for (Entity entity : allEntities) {
-            // Filter: must be living entity and not the shooter
             if (!(entity instanceof EntityLivingBase)) continue;
             if (entity == this.shootingEntity) continue;
             
             EntityLivingBase living = (EntityLivingBase) entity;
             
-            // Don't target dead entities
             if (living.getHealth() <= 0 || living.isDead) {
                 continue;
             }
             
-            // Don't target player's tamed pets
             if (this.shootingEntity instanceof EntityPlayer) {
                 if (living instanceof EntityTameable) {
                     EntityTameable tameable = (EntityTameable) living;
@@ -102,7 +99,6 @@ public abstract class EntityCustomArrow extends EntityArrow {
             }
         }
         
-        // If target found, adjust trajectory
         if (target != null) {
             Vec3d targetPos = new Vec3d(
                 target.posX,
@@ -113,30 +109,47 @@ public abstract class EntityCustomArrow extends EntityArrow {
             Vec3d arrowPos = new Vec3d(this.posX, this.posY, this.posZ);
             Vec3d direction = targetPos.subtract(arrowPos).normalize();
             
-            // Get current velocity
             Vec3d currentVelocity = new Vec3d(this.motionX, this.motionY, this.motionZ);
             double speed = currentVelocity.length();
             
-            // Steering strength based on level: 0.05, 0.08, 0.12
+            if (speed < 0.1D) return; // Don't steer if barely moving
+            
             float steeringStrength = 0.05F + (level * 0.03F);
             
-            // Interpolate between current direction and target direction
             Vec3d newDirection = currentVelocity.normalize()
                 .scale(1.0D - steeringStrength)
                 .add(direction.scale(steeringStrength))
                 .normalize();
             
-            // Apply new velocity
             this.motionX = newDirection.x * speed;
             this.motionY = newDirection.y * speed;
             this.motionZ = newDirection.z * speed;
             
-            // Update rotation to match new direction
             float yaw = (float) (MathHelper.atan2(newDirection.z, newDirection.x) * (180D / Math.PI)) - 90.0F;
             float pitch = (float) (-(MathHelper.atan2(newDirection.y, MathHelper.sqrt(newDirection.x * newDirection.x + newDirection.z * newDirection.z)) * (180D / Math.PI)));
             
             this.rotationYaw = yaw;
             this.rotationPitch = pitch;
+        }
+    }
+    
+    // Save bow stack to NBT
+    @Override
+    public void writeEntityToNBT(NBTTagCompound compound) {
+        super.writeEntityToNBT(compound);
+        if (!bowStack.isEmpty()) {
+            NBTTagCompound bowNBT = new NBTTagCompound();
+            bowStack.writeToNBT(bowNBT);
+            compound.setTag("BowStack", bowNBT);
+        }
+    }
+    
+    // Load bow stack from NBT
+    @Override
+    public void readEntityFromNBT(NBTTagCompound compound) {
+        super.readEntityFromNBT(compound);
+        if (compound.hasKey("BowStack")) {
+            this.bowStack = new ItemStack(compound.getCompoundTag("BowStack"));
         }
     }
 }
