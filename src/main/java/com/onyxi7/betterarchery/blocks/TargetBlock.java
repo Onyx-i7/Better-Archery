9873999626ab154bce1cd7bcf467fd795629f6e8
Onyx_i7 <Crackdroid69@gmail.com>
2026-07-09 10:29:50 -0400

    Refactor TargetBlock class for clarity and functionality

--- src/main/java/com/onyxi7/betterarchery/blocks/TargetBlock.java
+++ src/main/java/com/onyxi7/betterarchery/blocks/TargetBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.SoundType;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
