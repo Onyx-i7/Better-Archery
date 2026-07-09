7a0cd9e201425463d4bd87da654ec215e2af3529
Onyx_i7 <Crackdroid69@gmail.com>
2026-07-09 12:42:46 -0400

    Add import for Item in GunpowderBarrel.java

--- src/main/java/com/onyxi7/betterarchery/blocks/GunpowderBarrel.java
+++ src/main/java/com/onyxi7/betterarchery/blocks/GunpowderBarrel.java
@@ -7,6 +7,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.tileentity.TileEntity;
