31c15bb52bb09ec16bceebfc11dcb6e2459747f3
Onyx_i7 <Crackdroid69@gmail.com>
2026-07-09 12:07:14 -0400

    Add GunpowderBarrel to BlockInit

--- src/main/java/com/onyxi7/betterarchery/init/BlockInit.java
+++ src/main/java/com/onyxi7/betterarchery/init/BlockInit.java
@@ -1,6 +1,6 @@
 package com.onyxi7.betterarchery.init;
 
-import com.onyxi7.betterarchery.blocks.TargetBlock;
+import com.onyxi7.betterarchery.blocks.GunpowderBarrel;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
 
@@ -8,13 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BlockInit {
-    
     public static final List<Block> BLOCKS = new ArrayList<>();
     
     public static final Block TARGET_BLOCK = new TargetBlock();
+    public static final Block GUNPOWDER_BARREL = new GunpowderBarrel();
     
     static {
         BLOCKS.add(TARGET_BLOCK);
+        BLOCKS.add(GUNPOWDER_BARREL);
     }
     
     public static void registerItems() {
