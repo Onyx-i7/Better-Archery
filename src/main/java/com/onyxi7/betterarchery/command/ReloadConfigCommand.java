d54699b10a78e92af074b3f09e79eb43a6a17cb0
Onyx_i7 <crackdroid69@gmail.com>
2026-07-15 16:42:06 -0400

    Redundant code removed

--- src/main/java/com/onyxi7/betterarchery/command/ReloadConfigCommand.java
+++ src/main/java/com/onyxi7/betterarchery/command/ReloadConfigCommand.java
@@ -1,7 +1,6 @@
 package com.onyxi7.betterarchery.command;
 
 import com.onyxi7.betterarchery.config.BetterArcheryConfig;
-import com.onyxi7.betterarchery.entities.EntityQuiverSkeleton;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
@@ -29,32 +28,8 @@ public class ReloadConfigCommand extends CommandBase {
             // Reload settings
             net.minecraftforge.common.config.ConfigManager.sync("betterarchery", net.minecraftforge.common.config.Config.Type.INSTANCE);
             
-            // Re-register the Quiver Skeleton spawn
-            if (BetterArcheryConfig.mobs.enableQuiverSkeleton) {
-                EntityRegistry.addSpawn(
-                    EntityQuiverSkeleton.class,
-                    BetterArcheryConfig.mobs.quiverSkeletonSpawnWeight,
-                    1,
-                    1,
-                    EnumCreatureType.MONSTER,
-                    Biomes.PLAINS,
-                    Biomes.FOREST,
-                    Biomes.TAIGA,
-                    Biomes.DESERT,
-                    Biomes.JUNGLE,
-                    Biomes.BIRCH_FOREST,
-                    Biomes.ROOFED_FOREST,
-                    Biomes.EXTREME_HILLS,
-                    Biomes.SAVANNA,
-                    Biomes.MESA,
-                    Biomes.ICE_PLAINS,
-                    Biomes.COLD_TAIGA
-                );
-            }
-            
             sender.sendMessage(new TextComponentString("§a[BetterArchery] Config reloaded successfully!"));
             sender.sendMessage(new TextComponentString("§aDrill Max Blocks: " + BetterArcheryConfig.arrows.drillArrowMaxBlocks));
-            sender.sendMessage(new TextComponentString("§aQuiver Skeleton Enabled: " + BetterArcheryConfig.mobs.enableQuiverSkeleton));
         } else {
             sender.sendMessage(new TextComponentString("§cUsage: " + getUsage(sender)));
         }
